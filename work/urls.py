b6651de6d02d89d9f3229d21f8b0f9a3bb1adf49
Lancelot HARDEL <lancelot@hardel.me>
2014-05-12 04:38:55 +0200

    Bim.

--- work/urls.py
+++ work/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from work import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^critique/(?P<type_of_work>.+)/(?P<slug>.+)/$', views.review_detail, name='review_detail'),
+    url(r'^oeuvre/(?P<type_of_work>.+)/(?P<slug>.+)$', views.work_detail, name='work_detail'),
+
 )
--- work/views.py
+++ work/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from work.models import *
+from django.http import Http404
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
+
+def work_detail(request, type_of_work, slug):
+    try:
+        work = Work.objects.get(slug=slug)
+    except Work.DoesNotExist:
+        raise Http404
+    return render(request, 'work/detail.html', {'work': work})
+
+def review_detail(request, type_of_work, slug):
+    try:
+        review = StaffReview.objects.get(slug=slug)
+    except Work.DoesNotExist:
+        raise Http404
+    return render(request, 'work/review.html', {'review': review})
     
 # Create your views here.
