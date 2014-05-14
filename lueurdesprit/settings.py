1b9dcb59b5186ddc5514279c9e9873a2f48775a1
Lancelot HARDEL <git@hardel.me>
2014-05-14 16:29:22 +0200

    Added MySQL

--- lueurdesprit/settings.py
+++ lueurdesprit/settings.py
@@ -74,6 +74,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lueurdesprit',
+        'USER': 'lueurdesprit',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
