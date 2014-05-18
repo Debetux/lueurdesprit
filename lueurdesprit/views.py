04d9a6f0a033302116a3411ab818b7426d599c0b
Lancelot HARDEL <git@hardel.me>
2014-05-18 10:30:15 +0200

    few changes on homepage

--- lueurdesprit/views.py
+++ lueurdesprit/views.py
@@ -4,13 +4,23 @@ from work.models import TypeOfWork, Work, StaffReview
 from blog.models import Entry
 
 def home(request):
-    latest_work_pub = Work.objects.all().exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
-    latest_reviews_pub = StaffReview.objects.all().exclude(work__poster='').order_by('-pub_date')[:2]
+	""" Useless, for now """
+    # latest_work_pub = Work.objects.all().exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
+    # latest_reviews_pub = StaffReview.objects.all().exclude(work__poster='').order_by('-pub_date')[:2]
     latest_news = Entry.objects.all()
 
+    latest_staffreviews_theatre = Work.objects.filter(work__type_of_work__slug="piece-de-theatre").exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
+    latest_staffreviews_book = Work.objects.filter(work__type_of_work__slug="litterature").exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
+    latest_staffreviews_cinema = Work.objects.filter(work__type_of_work__slug="cinema").exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
+    latest_staffreviews_music = Work.objects.filter(work__type_of_work__slug="musique").exclude(staffreview__pk__isnull=True).order_by('-pk')[:4]
+
     context = {
-        'latest_work_pub': latest_work_pub,
-        'latest_reviews_pub': latest_reviews_pub,
-        'latest_news': latest_news,
+        'latest_work_pub': 				latest_work_pub,
+        'latest_reviews_pub': 			latest_reviews_pub,
+        'latest_news': 					latest_news,
+        'latest_staffreviews_theatre': 	latest_staffreviews_theatre,
+        'latest_staffreviews_cinema': 	latest_staffreviews_cinema,
+        'latest_staffreviews_book': 	latest_staffreviews_book,
+        'latest_staffreviews_music': 	latest_staffreviews_music,
     }
     return render(request, 'home/home.html', context)
