1c11d703129fd364382507305bcf046a56f36b06
Lancelot HARDEL <git@hardel.me>
2014-05-18 10:14:11 +0200

    Trying things

--- lueurdesprit/views.py
+++ lueurdesprit/views.py
@@ -4,7 +4,7 @@ from work.models import TypeOfWork, Work, StaffReview
 from blog.models import Entry
 
 def home(request):
-    latest_work_pub = Work.objects.all().exclude(poster='').order_by('-pk')[:4]
+    latest_work_pub = Work.objects.all().exclude(staffreview__pk='').order_by('-pk')
     latest_reviews_pub = StaffReview.objects.all().exclude(work__poster='').order_by('-pub_date')[:2]
     latest_news = Entry.objects.all()
 
